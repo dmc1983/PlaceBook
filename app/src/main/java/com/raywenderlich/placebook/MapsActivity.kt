877069cee4793693857a90f5802064b737e8e7a2
Daryl Cornelius <69173478+dmc1983@users.noreply.github.com>
2021-09-18 18:36:46 -0400

    Chapter 15: Manifest merger failed

--- app/src/main/java/com/raywenderlich/placebook/MapsActivity.kt
+++ app/src/main/java/com/raywenderlich/placebook/MapsActivity.kt
@@ -40,98 +40,30 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
 
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
         setupLocationClient()
         setupPlacesClient()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        getCurrentLocation()
-
         map.setInfoWindowAdapter(BookmarkInfoWindowAdapter(this))
-
+        getCurrentLocation()
         map.setOnPoiClickListener {
             displayPoi(it)
-
         }
     }
+
     private fun setupPlacesClient() {
-        Places.initialize(applicationContext,
-            getString(R.string.google_maps_key))
-        placesClient = Places.createClient(this)
+        Places.initialize(applicationContext, getString(R.string.google_maps_key));
+        placesClient = Places.createClient(this);
     }
 
-
-
-    private fun setupLocationClient(){
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-    }
-
-    private fun requestLocationPermissions(){
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-        REQUEST_LOCATION)
-    }
-
-    companion object{
-        private const val REQUEST_LOCATION = 1
-        private const val TAG = "MapsActivity"
-    }
-
-
-    private fun getCurrentLocation(){
-
-
-        if(ActivityCompat.checkSelfPermission(this,
-            Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED) {
-            requestLocationPermissions()
-        }else{
-
-            }
-            map.isMyLocationEnabled = true
-            fusedLocationClient.lastLocation.addOnCompleteListener{
-                val location = it.result
-                if(location != null) {
-                    val latLng = LatLng(
-                        location.latitude,
-                        location.longitude
-                    )
-
-
-                    val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
-
-                    map.moveCamera(update)
-
-
-                }else{
-                    Log.e(TAG, "No location found")
-                }
-            }
-
-        }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-        grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == REQUEST_LOCATION) {
-
-            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getCurrentLocation()
-            } else {
-                Log.e(TAG, "Location permission denied")
-            }
-        }
-
-    }
     private fun displayPoi(pointOfInterest: PointOfInterest) {
         displayPoiGetPlaceStep(pointOfInterest)
     }
@@ -139,15 +71,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun displayPoiGetPlaceStep(pointOfInterest:
                                        PointOfInterest) {
         val placeId = pointOfInterest.placeId
+
         val placeFields = listOf(Place.Field.ID,
             Place.Field.NAME,
             Place.Field.PHONE_NUMBER,
             Place.Field.PHOTO_METADATAS,
             Place.Field.ADDRESS,
             Place.Field.LAT_LNG)
+
         val request = FetchPlaceRequest
             .builder(placeId, placeFields)
             .build()
+
         placesClient.fetchPlace(request)
             .addOnSuccessListener { response ->
                 val place = response.place
@@ -162,44 +97,82 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
     }
-    private fun displayPoiGetPhotoStep(place: Place) {
 
-        val photoMetadata = place
-            .getPhotoMetadatas()?.get(0)
+    private fun displayPoiGetPhotoStep(place: Place) {
+        val photoMetadata = place.getPhotoMetadatas()?.get(0)
         if (photoMetadata == null) {
             displayPoiDisplayStep(place, null)
             return
         }
-
-        val photoRequest = FetchPhotoRequest
-            .builder(photoMetadata)
-            .setMaxWidth(resources.getDimensionPixelSize(
-                R.dimen.default_image_width))
-            .setMaxHeight(resources.getDimensionPixelSize(
-                R.dimen.default_image_height))
+        val photoRequest = FetchPhotoRequest.builder(photoMetadata)
+            .setMaxWidth(resources.getDimensionPixelSize(R.dimen.default_image_width))
+            .setMaxHeight(resources.getDimensionPixelSize(R.dimen.default_image_height))
             .build()
-
-        placesClient.fetchPhoto(photoRequest)
-            .addOnSuccessListener { fetchPhotoResponse ->
-                val bitmap = fetchPhotoResponse.bitmap
-                displayPoiDisplayStep(place, bitmap)
-            }.addOnFailureListener { exception ->
-                if (exception is ApiException) {
-                    val statusCode = exception.statusCode
-                    Log.e(TAG,
-                        "Place not found: " +
-                                exception.message + ", " +
-                                "statusCode: " + statusCode)
-                }
+        placesClient.fetchPhoto(photoRequest).addOnSuccessListener { fetchPhotoResponse ->
+            val bitmap = fetchPhotoResponse.bitmap
+            displayPoiDisplayStep(place, bitmap)
+        }.addOnFailureListener { exception ->
+            if (exception is ApiException) {
+                val statusCode = exception.statusCode
+                Log.e(TAG, "Place not found: " + exception.message + ", statusCode: " + statusCode)
             }
+        }
     }
-    private fun displayPoiDisplayStep(place: Place, photo: Bitmap?)
-    {
-        map.addMarker(MarkerOptions()
+
+    private fun displayPoiDisplayStep(place: Place, photo: Bitmap?) {
+        val marker = map.addMarker(MarkerOptions()
             .position(place.latLng as LatLng)
             .title(place.name)
             .snippet(place.phoneNumber)
         )
+        marker?.tag = photo
+    }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation()
+            } else {
+                Log.e(TAG, "Location permission denied")
+            }
+        }
+    }
+
+    private fun setupLocationClient() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun getCurrentLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermissions()
+        } else {
+            map.isMyLocationEnabled = true
+
+            fusedLocationClient.lastLocation.addOnCompleteListener {
+                val location = it.result
+                if (location != null) {
+                    val latLng = LatLng(location.latitude, location.longitude)
+                    val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
+                    map.moveCamera(update)
+                } else {
+                    Log.e(TAG, "No location found")
+                }
+            }
+        }
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_LOCATION)
+    }
+
+    companion object {
+        private const val REQUEST_LOCATION = 1
+        private const val TAG = "MapsActivity"
     }
 }
